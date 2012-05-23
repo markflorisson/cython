3e2e19dcc747a55d091c4f6e0de31df254e1223d
Mark Florisson <markflorisson88@gmail.com>
2012-05-23 11:19:30 +0100

    Fix python 3 import

--- Cython/Compiler/PyrexTypes.py
+++ Cython/Compiler/PyrexTypes.py
@@ -7,7 +7,6 @@ import StringEncoding
 import Naming
 import copy
 from Errors import error
-import Symtab
 
 class BaseType(object):
     #
@@ -598,7 +597,7 @@ class MemoryViewSliceType(PyrexType):
         return True
 
     def get_entry(self, node, cname=None, type=None):
-        import MemoryView
+        import MemoryView, Symtab
 
         if cname is None:
             assert node.is_simple() or node.is_temp or node.is_elemental
